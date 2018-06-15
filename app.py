35839d59117272554dc83832f218453d53133cc7
Timothy Willard <timothywillard22@gmail.com>
2018-06-15 16:01:17 -0400

    Added the ability to use Dash's http basic auth

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import dash
+import dash_auth
 
-app = dash.Dash()
+from config import config as cfg
+
+app = None
+auth = None
+if cfg["auth"]["required"]:
+    app = dash.Dash("auth")
+    auth = dash_auth.BasicAuth(app, cfg["auth"]["validPairs"])
+else:
+    app = dash.Dash()
+
 server = app.server
 app.config.suppress_callback_exceptions = True
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
--- config.py
+++ config.py
@@ -1,14 +1,21 @@
 config = {
-  "serverName": "Dash Multi-App Server",
-  "debug": True,
-  "otherLinkName": None,
-  "otherLinkHref": "#",
-  "apps": [
-    {
-      "name": "app1",
-      "title": "Application 1",
-      "path": "app-one",
-      "description": "This is a simple example application."
+    "serverName": "Dash Multi-App Server",
+    "debug": True,
+    "otherLinkName": None,
+    "otherLinkHref": "#",
+    "apps": [
+        {
+            "name": "app1",
+            "title": "Application 1",
+            "path": "app-one",
+            "description": "This is a simple example application."
+        }
+    ],
+    "auth": {
+        "required": False,
+        "validPairs": [
+            ["admin", "admin"],
+            ["username", "password"]
+        ]
     }
-  ]
 }
